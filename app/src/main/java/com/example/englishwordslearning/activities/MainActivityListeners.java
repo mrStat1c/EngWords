559a49303c5c29908010542a46ce39f48daf90b4
mrStat1c <amelin.ilya@gmail.com>
2024-03-26 22:31:56 +0300

    1.3.3

--- app/src/main/java/com/example/englishwordslearning/activities/MainActivityListeners.java
+++ app/src/main/java/com/example/englishwordslearning/activities/MainActivityListeners.java
@@ -5,18 +5,23 @@ import static com.example.englishwordslearning.Constants.RED;
 import static com.example.englishwordslearning.Constants.YELLOW;
 
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.englishwordslearning.DbHelper;
 import com.example.englishwordslearning.Dictionary;
 
+import java.util.Locale;
+
 /**
  * Предоставляет обработчики событий для элементов MainActivity
  */
-public class MainActivityListeners {
+public class MainActivityListeners implements TextToSpeech.OnInitListener {
 
     private MainActivity activity;
+    private TextToSpeech mTextToSpeech;
+    private boolean mIsInit;
 
     public MainActivityListeners(MainActivity activity) {
         this.activity = activity;
@@ -27,11 +32,17 @@ public class MainActivityListeners {
      */
     View.OnClickListener getTranslateClickListener(TextView rusTextView) {
         return x -> {
-            String engWord = this.activity.getEngText();
-            rusTextView.setText(String.format("%s%s%s",
-                    Dictionary.getTranslation(engWord),
-                    "\n------------------\n",
-                    Dictionary.getTranscription(engWord)));
+            if (rusTextView.getText().toString().isEmpty()) {
+                String engWord = this.activity.getEngText();
+                rusTextView.setText(String.format("%s%s%s",
+                        Dictionary.getTranslation(engWord),
+                        "\n------------------\n",
+                        Dictionary.getTranscription(engWord)));
+            } else {
+                //todo doesn't work. Speak() returns -1 (ERROR)
+                this.mTextToSpeech = new TextToSpeech(this.activity, this);
+                this.mTextToSpeech.speak("Hello", TextToSpeech.QUEUE_FLUSH, null, "someId");
+            }
         };
     }
 
@@ -87,4 +98,23 @@ public class MainActivityListeners {
             this.activity.startActivity(infoActivityIntent);
         };
     }
+
+    /**
+     * @param status TextToSpeech.OnInitListener implementation
+     */
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            Locale locale = new Locale("eng");
+            int result = mTextToSpeech.setLanguage(locale);
+            if (result == TextToSpeech.LANG_MISSING_DATA) {
+                this.mIsInit = false;
+            } else {
+                this.mIsInit = true;
+            }
+        } else {
+            this.mIsInit = false;
+        }
+    }
+
 }
